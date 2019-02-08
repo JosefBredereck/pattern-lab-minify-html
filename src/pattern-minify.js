a71cdcbc968ed0fd0020101e161fbd9f714251e6
Josef Bredereck <josef.bredereck@acando.de>
2019-02-08 20:41:54 +0100

    Fix: undefined error causes patternlab start lock

--- src/pattern-minify.js
+++ src/pattern-minify.js
@@ -16,41 +16,46 @@ const fsExtra = require('fs-extra');
  */
 module.exports = function(patternlab, pattern) {
 
-  if (!patternlab) {
-    console.error(pluginName + ': patternlab object not provided to findTab');
-    process.exit(1);
-  }
-
-  if (!pattern) {
-    console.error(pluginName + ': pattern object not provided to findTab');
-    process.exit(1);
-  }
-
-  let options = patternlab.config.plugins[pluginName].options;
-  let pluginOptions = patternlab.config.plugins[pluginName].pluginOptions;
-
-  if (pluginOptions.verbose) {
-    console.log('Starting to optimize HTML of ' + pattern.patternPartial + '...');
-  }
-
-  const paths = patternlab.config.paths;
-  const makePath = type => path.join(paths.public.patterns, pattern.getPatternLink(patternlab, type));
-
-  const files = [makePath('rendered'), makePath('markupOnly')];
-
-  files.forEach(file => {
-    let source = fs.readFileSync(file, 'utf8');
-    let result = options.minify ? minifier(source, options.minify) : source;
-
-    if (options.beautify)
-      result = beautifyHtml(result, options.beautify);
-
-    if (pluginOptions.verbose) {
-      console.log('Processed ' + pattern.patternPartial +
-        ', before: ' + source.length + ', after: ' + result.length +
-        ', ratio: ' + Math.round(((result.length * 100) / source.length) * 100) / 100 + '%');
+    if (!patternlab) {
+        console.error(pluginName + ': patternlab object not provided');
+        process.exit(1);
     }
 
-    fsExtra.outputFileSync(file, result);
-  });
+    if (!pattern) {
+        console.error(pluginName + ': pattern object not provided');
+        process.exit(1);
+    }
+
+    let options = patternlab.config.plugins[pluginName].options;
+    let pluginOptions = options.pluginOptions;
+
+    if (!pluginOptions || !options) {
+        console.error(pluginName + ': missing Plugin options');
+        process.exit(1);
+    }
+
+    if (pluginOptions.verbose) {
+        console.log('Starting to optimize HTML of ' + pattern.patternPartial + '...');
+    }
+
+    const paths = patternlab.config.paths;
+    const makePath = type => path.join(paths.public.patterns, pattern.getPatternLink(patternlab, type));
+
+    const files = [makePath('rendered'), makePath('markupOnly')];
+
+    files.forEach(file => {
+        let source = fs.readFileSync(file, 'utf8');
+        let result = options.minify ? minifier(source, options.minify) : source;
+
+        if (options.beautify)
+            result = beautifyHtml(result, options.beautify);
+
+        if (pluginOptions.verbose) {
+            console.log('Processed ' + pattern.patternPartial +
+                ', before: ' + source.length + ', after: ' + result.length +
+                ', ratio: ' + Math.round(((result.length * 100) / source.length) * 100) / 100 + '%');
+        }
+
+        fsExtra.outputFileSync(file, result);
+    });
 };
